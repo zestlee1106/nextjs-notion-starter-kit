4036ecc06f9e2c5c39927773b2ffd0d0ce1b6d51
이영우 <ywlee@hanatour.com>
2022-12-05 16:55:17 +0900

    문법 오류 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -66,23 +66,24 @@ export default function App({ Component, pageProps }: AppProps) {
     }
   }, [router.events])
 
-  return 
-  <>
-    <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`} />
-    <Script
-      id="gtag-init"
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${gtag.GA_TRACKING_ID}', {
-            page_path: window.location.pathname,
-          });
-        `
-      }}
-    />
-    <Component {...pageProps} />
-  </>
+  return (
+    <>
+      <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`} />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
